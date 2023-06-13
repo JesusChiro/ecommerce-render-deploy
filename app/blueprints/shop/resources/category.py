fe80cd4c6a076223ea6765135cab0ff06c737d2b
JesusChiro <jesus_chiroque@hotmail.com>
2023-06-13 16:38:45 -0500

    first commit

---
+++ app/blueprints/shop/resources/category.py
@@ -0,0 +1,66 @@
+import os
+import werkzeug
+
+from flask_restful import Resource, Api, reqparse
+from flask import request
+
+from .. import shop
+
+from ..models import Category
+from ..schemas import CategorySchema
+
+from flask_jwt_extended import jwt_required
+
+api = Api(shop)
+
+
+class CategoryResource(Resource):
+    
+    def get(self):
+        data = Category.get_all()
+        data_schema = CategorySchema(many=True)
+        context = {"status": True, "content": data_schema.dump(data)}
+
+        return context
+
+    @jwt_required()
+    def post(self):
+        data = request.get_json()
+        name = data["name"]
+
+        new_category = Category(name)
+        new_category.save()
+
+        data_schema = CategorySchema()
+
+        context = {"status": True, "content": data_schema.dump(new_category)}
+
+        return context
+
+    @jwt_required()
+    def put(self, id):
+        data = request.get_json()
+        name = data["name"]
+
+        upd_Category = Category.get_by_id(id)
+        upd_Category.name = name
+        upd_Category.save()
+
+        data_schema = CategorySchema()
+
+        context = {"status": True, "content": data_schema.dump(upd_Category)}
+
+        return context
+
+    # @jwt_required()
+    def delete(self, id):
+        del_category = Category.get_by_id(id)
+        del_category.delete()
+
+        context = {"status": True, "content": "registro eliminado"}
+
+        return context
+
+
+api.add_resource(CategoryResource, "/category")
+api.add_resource(CategoryResource, "/category/<id>", endpoint="category")
